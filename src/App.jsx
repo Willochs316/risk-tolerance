5b2788572ee29add6696e85ce24a9aa3bb504337
Willochs316 <willochs316@gmail.com>
2023-09-03 15:41:23 +0100

    initialize project for risk scores

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-500 underline">
+      hello tailwind project for risk scores
+    </div>
+  );
+}
+
+export default App;
