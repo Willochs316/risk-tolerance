305285cf4aef31df9b763852aa4adfdc684938c9
Willochs316 <willochs316@gmail.com>
2023-09-03 23:56:16 +0100

    add meta data for sheet

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import { google } from "googleapis";
 import colors from "colors";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -17,6 +18,29 @@ app.use(
 );
 
 app.use(cors());
+
+app.get("/", async (req, res) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "stock-risk.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "10Nk9gaFZF_1QmNx7Me8YOS0rODCMCDVVfqxWuzqsLq8";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  res.send(metaData.data);
+});
 
 const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
